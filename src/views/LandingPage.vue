a1eb2c831e7330265dcdc5643216d2ce81a8242d
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-09-30 17:01:01 -0300

    Fix things

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  created() {
+    this.getHamburguer();
+  },
+  methods: {
+    async getHamburguer() {
+      fetch('https://tec-burger-default-rtdb.firebaseio.com/hamburguer.json').then( (response) => {
+        if(response.ok) {
+          return response.json();
+        }
+      }).then((data) => {
+        console.log(data)
+      })
+    }
+  },
+}
+</script>
